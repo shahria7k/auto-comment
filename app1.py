accf24a23ada9aeabb0320e8d475ab5ff1aa7adf
S Jaman Khan <41187226+imsj7k@users.noreply.github.com>
2020-06-17 09:55:57 +0600

    Initial commit

---
+++ app1.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+
+comments = ["hi"," Logigtech is awesomome"," checking my python comment bot"," Pyhton is  awesome"," I am a messy proggramer", "i am just checking my python skill",]
+
+time.sleep(10)
+for i in range(1000):
+    pyautogui.typewrite(comments[i%6])
+    pyautogui.typewrite("\n")
+    time.sleep(0.5)
